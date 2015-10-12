204192a23d84193d942a851cf8363ad3e9a5a22b
Samu Juvonen <samu.juvonen@gmail.com>
2015-10-12 23:27:24 +0300

    Remove old modules

--- ext/pagetitle.js
+++ ext/pagetitle.js
@@ -1,57 +1,109 @@
-/**
- * Fetches page titles from links that are sent to channels.
- */
-
 "use strict";
 
 let entities = require("entities");
 let iconv = require("iconv-lite");
-let Promise = require("promise");
+let net = require("../core/net");
+let proxy = require("../core/proxy.js");
+let zlib = require("zlib");
 
-exports.initialize = function(bot) {
-  PageTitle.net = bot.net;
-  bot.on("message", function(message) {
-    let url = PageTitle.parseUrl(message.content);
+class PageTitle {
+  constructor(connection) {
+    this.connection = connection;
+    this.connection.events.on("message", proxy(this.onMessage, this));
+  }
 
+  onMessage(message) {
+    let connection = this.connection;
+    let url = this.parseUrl(message.content);
     if (url) {
-      PageTitle.fetchPage(url).then(function(title) {
-        message.reply(title);
-      }).catch(function(error) {
-        console.log("ERROR", error);
+      this.fetchTitle(url).then(title => {
+        message.reply("> " + title);
+      }, error => {
+        console.error("Failed to fetch page title:", error);
       });
     }
-  });
-};
+  }
 
-let PageTitle = {
-  parseUrl: function(text) {
-    let result = text.match(/(http.{0,1}:\/\/|www\.)\S+/);
-    return result ? result[0] : null;
-  },
-  fetchPage: function(url) {
-    return new Promise(function(resolve, reject) {
-      PageTitle.net.download(url).then(function(response) {
-        if (!("content-type" in response.headers) || !response.headers["content-type"].match("html")) {
-          return reject("Invalid content type");
+  fetchTitle(url) {
+    let pagetitle = this;
+    return new Promise((resolve, reject) => {
+      let request = net.request(url);
+      request.on("response", response => {
+        let headers = response.headers;
+        if (!("content-type" in headers && headers["content-type"].match("html"))) {
+          request.abort();
         }
-        // PageTitle.net.download(url).then(function(response) {
-          let charset = response.headers["content-type"].match(/charset=\b(.+)\b/);
-          charset = charset ? charset[1] : "UTF-8";
+      });
 
-          if (charset) {
-            let data = iconv.decode(response.data, charset);
-            let title = PageTitle.parseTitle(data);
-
-            if (title) {
-              return resolve("> " + title);
-            }
+      request.start().then(response => {
+        this.decodeResponse(response).then(content => {
+          let title = this.parseTitle(content);
+          if (title) {
+            resolve(title);
           }
-        // });
+        });
       });
     });
-  },
-  parseTitle: function(html) {
-    let title = html.match(/<title.*?>(.+)<\/title>/i);
-    return title ? entities.decodeHTML(title[1]) : null;
-  },
+  }
+
+  decodeResponse(response) {
+    return new Promise((resolve, reject) => {
+      let decode = function(content) {
+        let charset = response.headers["content-type"].match(/charset=\b(.+)\b/);
+        charset = charset ? charset[1] : "UTF-8";
+        if (charset) {
+          let data = iconv.decode(content.slice(0, 800), charset);
+          resolve(data);
+        }
+      }
+      if (response.headers["content-encoding"] == "gzip") {
+        zlib.gunzip(response.data, (error, raw) => {
+          decode(raw);
+        });
+      } else {
+        decode(response.data);
+      }
+
+    })
+  }
+
+  parseUrl(content) {
+    let result = content.match(this.urlRegex);
+    return result ? result[0] : null;
+  }
+
+  parseTitle(html) {
+    let start = html.search(this.titleOpenRegex);
+    let end = html.search(this.titleCloseRegex);
+
+    if (start >= 0 && end >= 0) {
+      let title = html.substring(html.indexOf(">", start) + 1, end).replace(/\s+/, " ").trim();
+      return entities.decodeHTML(title);
+    }
+  }
+
+  get urlRegex() {
+    if (!this._ur) {
+      this._ur = /(http.{0,1}:\/\/|www\.)\S+/;
+    }
+    return this._ur;
+  }
+
+  get titleOpenRegex() {
+    if (!this._otr) {
+      this._otr = /<title/i;
+    }
+    return this._otr;
+  }
+
+  get titleCloseRegex() {
+    if (!this._ctr) {
+      this._ctr = /<\/title>/i;
+    }
+    return this._ctr;
+  }
+}
+
+exports.configure = function(connection) {
+  return new PageTitle(connection);
 };
