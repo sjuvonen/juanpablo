70b29c5168e5cd73d946d33225ee6c0a3f5656e4
Samu Juvonen <samu.juvonen@gmail.com>
2016-04-17 20:00:48 +0300

    Use Ergast API instead of parsing Wikipedia

--- src/modules/seasondata.js
+++ src/modules/seasondata.js
@@ -13,7 +13,13 @@ let SeasonSchema = new mongoose.Schema({
   //   number: Number,
   //   name: String,
   // }],
-  drivers: [String],
+  drivers: [{
+    firstName: String,
+    lastName: String,
+    code: String,
+    number: Number,
+    _id: false,
+  }],
   points: {
     type: [Number],
     default: DefaultPoints
@@ -34,109 +40,17 @@ SeasonSchema.statics.findCurrent = function() {
 
 let Season = mongoose.model("season", SeasonSchema);
 
-class WikipediaDriverParser {
-  parse(html) {
-    console.log("PARSE");
-    let State = {
-      WAIT: 0,
-      WAIT_TITLE_SPAN: 1,
-      WAIT_LEGEND_TABLE: 2,
-      WAIT_RESULTS_TABLE: 3,
-      RESULTS_TABLE: 4,
-      CELL_NAME: 5,
-      SKIP_FLAG_ANCHOR: 6,
-      READ_NAME: 7,
-      CELL_RESULT: 8,
-    };
-    let target_round = 0;
-
-    let drivers = [];
-    let result = [];
-    let cell_i = 0;
-    let name = null;
-    let state = State.WAIT;
-
-    try {
-      let parser = new htmlparser.Parser({
-        onopentag: (tag, attrs) => {
-          switch (state) {
-            case State.WAIT:
-              if (tag == "h3") {
-                state = State.WAIT_TITLE_SPAN;
-              }
-              break;
-            case State.WAIT_TITLE_SPAN:
-              if (tag == "span" && attrs.id && attrs.id.match(/drivers[\w\.]+standings/i)) {
-                state = State.WAIT_LEGEND_TABLE;
-              }
-              break;
-            case State.WAIT_LEGEND_TABLE:
-            case State.WAIT_RESULTS_TABLE:
-              if (tag == "table") {
-                state++;
-              }
-              break;
-            case State.RESULTS_TABLE:
-              if (tag == "td") {
-                state++;
-              }
-              break;
-            case State.CELL_NAME:
-            case State.SKIP_FLAG_ANCHOR:
-              if (tag == "a") {
-                state++;
-              }
-              break;
-            case State.CELL_RESULT:
-              if (tag == "td") {
-                cell_i++;
-              }
-              break;
-          }
-        },
-        ontext: text => {
-          switch (state) {
-            case State.READ_NAME:
-              name = text.trim();
-              drivers.push(text.trim());
-              break;
-            case State.CELL_RESULT:
-              if (cell_i == target_round && text.trim().length) {
-                let pos = parseInt(text.trim());
-                if (pos) {
-                  result[pos-1] = name;
-                }
-              }
-              break;
-          }
-        },
-        onclosetag: tag => {
-          switch (state) {
-            case State.RESULTS_TABLE:
-              if (tag == "table") {
-                state = -1;
-              }
-              break;
-            case State.READ_NAME:
-              if (tag == "a") {
-                state = State.CELL_RESULT;
-                cell_i = 0;
-              }
-              break;
-            case State.CELL_RESULT:
-              if (tag == "tr") {
-                state = State.RESULTS_TABLE;
-              }
-              break;
-          }
-        },
-      });
-      parser.write(html);
-      parser.end();
-
-      return drivers;
-    } catch (error) {
-      console.error("seasondata.parse:", error.stack);
+class ErgastParser {
+  parse(json) {
+    let data = JSON.parse(json).MRData.DriverTable;
+    return {
+      year: parseInt(data.season),
+      drivers: data.Drivers.map(d => ({
+        number: parseInt(d.permanentNumber),
+        firstName: d.givenName,
+        lastName: d.familyName,
+        code: d.code,
+      })),
     }
   }
 }
@@ -147,22 +61,12 @@ exports.configure = services => {
       year = (new Date).getFullYear();
     }
 
-    let url = services.get("config").get("modules.seasondata.source.wikipedia");
+    let url = services.get("config").get("modules.seasondata.source.ergast");
     let source = util.format(url, year);
 
     return net.download(source).then(result => {
-      let parser = new WikipediaDriverParser;
-      let drivers = parser.parse(result.data.toString());
-
-      drivers.sort();
-
-      let entry = {
-        _id: year,
-        drivers: drivers,
-        points: DefaultPoints,
-      };
-
-      return Season.update({_id: year}, {$set: entry}, {upsert: true});
+      let season = (new ErgastParser).parse(result.data);
+      return Season.update({_id: season.year}, season, {upsert: true});
     });
   }
 
